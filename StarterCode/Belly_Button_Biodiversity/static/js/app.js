8b9c4bd4ace0c15e0239d1565ab3ca02fe107677
Anika Johnson <anikaleejohnson@gmail.com>
2020-03-28 14:09:36 -0500

    final update

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -1,4 +1,3 @@
-// 1. Use the D3 library to read in `samples.json`.
 
 // Stub - test to confirm that you called it
 function DrawBargraph(sampleId)
@@ -179,18 +178,3 @@ function InitDashboard()
 }
 
 InitDashboard();
-// 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-
-    // *Use `sample_values` as the values for the bar chart.
-    // *Use `otu_ids` as the labels for the bar chart.
-
-    // *Use `otu_labels` as the hovertext for the chart.
-
-// 3. Create a bubble chart that displays each sample.
-
-
-// 4. Display the sample metadata, i.e., an individual's demographic information.
-
-// 5. Display each key-value pair from the metadata JSON object somewhere on the page.
-
-// 6. Update all of the plots any time that a new sample is selected.
